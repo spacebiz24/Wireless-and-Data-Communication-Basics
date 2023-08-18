eec74a1606ceb219d2e6789575c14df5ebb1d748
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-08-18 22:46:27 +0530

    Create P1-4.c

---
+++ P1-4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <conio.h>
+
+void main()
+{
+    // Setup
+    char Message[50];
+    char Scambled[50];
+    short int Shift;
+    printf("Enter a string of letters: ");
+    strupr(gets(Message));
+
+    // Encryption
+    printf("Enter Encryption Key: ");
+    scanf("%d", &Shift);
+    Shift %= 26;
+    for (int i = 0; i < strlen(Message); i++)
+        if (isalpha(Message[i]))
+            for (int j = 0; j < Shift; j++)
+            {
+                if (Message[i] == 'Z')
+                    Message[i] = 'A';
+                else
+                    Message[i]++;
+            }
+    printf("Scrambled text: %s\n", Message);
+
+    // Decryption
+    printf("Enter Decryption Key: ");
+    scanf("%d", &Shift);
+    Shift %= 26;
+    for (int i = 0; i < strlen(Message); i++)
+        if (isalpha(Message[i]))
+            for (int j = 0; j < Shift; j++)
+            {
+                if (Message[i] == 'A')
+                    Message[i] = 'Z';
+                else
+                    Message[i]--;
+            }
+    printf("Original Message: %s\n", Message);
+    getch();
+}
