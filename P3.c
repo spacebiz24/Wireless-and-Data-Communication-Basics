b352665973f681671a9bd7d0007f5cbbc634cdd0
Siddhaanth S Iyer <78657717+spacebiz24@users.noreply.github.com>
2023-08-26 21:20:46 +0530

    Create P3.c

---
+++ P3.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+
+#define DLE 16
+#define STX 2
+#define ETX 3
+
+char inputCharacter, stuffedData[50], deStuffedData[50];
+char Preamble[] = {DLE, STX}, Postamble[] = {DLE, ETX};
+int length = 2;
+
+void stuffData()
+{
+    printf("\nEnter the data stream:\n");
+    printf("CTRL+P -> DLE\nCTRL+B -> STX\nCTRL+C -> ETX\n");
+    strcat(stuffedData, Preamble);
+    while ((inputCharacter = getche()) != '\r')
+    {
+        if (inputCharacter == DLE)
+            stuffedData[length++] = DLE;
+        stuffedData[length++] = inputCharacter;
+    }
+    strcat(stuffedData, Postamble);
+    printf("\nCharacter Stuffed Stream:\n");
+    for (int i = 0; i < length + 2; i++)
+        printf("%c", stuffedData[i]);
+}
+
+void destuffData()
+{
+    printf("\nDe-Stuffed Stream:\n");
+    for (int j = 2, k = 0; j, k < length - 2; j++, k++)
+    {
+        if (stuffedData[j] == DLE)
+            ++j;
+        deStuffedData[k] = stuffedData[j];
+    }
+    for (int i = 0; i < length - 2; i++)
+        printf("%c", deStuffedData[i]);
+}
+
+void main()
+{
+    stuffData();
+    destuffData();
+}
