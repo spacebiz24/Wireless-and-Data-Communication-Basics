380f0fac9e47e55b0391bbb483bec0a180295e55
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-13 22:21:41 +0530

    Update Log_Distance_Shadow.m

--- MATLAB/Log_Distance_Shadow.m
+++ MATLAB/Log_Distance_Shadow.m
@@ -1,18 +1,21 @@
-% Analysis of Log-distance path loss propagation model (w/ Shadow)
+% Analysis of Log-distance path loss propagation model (w/ & w/o Shadow)
 clc
 close all;
+with_shadowing = 0;
 Fixed_Distance = input("Enter the reference distance: ");
 Arbitrary_Distance = 1000 : 1000 : 20000;
 % Values must be between 1.6 to 6
 Path_Loss_Exponent = rand(1,12)*(6-1.6) + 1.6;
 Frequency = randi([900, 4000], 1,12);
 Sigma = rand(1,12)*(15-3) + 3;
+X = zeros(1,12);
 
 for i = 1:12
     Lambda(i) = 3e8 / (Frequency(i) * 10 ^ 6);
     PL_D0(i) = -10 * log10((Lambda(i) ^ 2) / ((4 * pi * Fixed_Distance) ^ 2));
-    X(i) = normrnd(0, sigma(i));
-    disp(X(i));
+    if(with_shadowing)
+        X(i) = normrnd(0, sigma(i));
+    end
 end
 for i = 1:12
     for j = 1:20
