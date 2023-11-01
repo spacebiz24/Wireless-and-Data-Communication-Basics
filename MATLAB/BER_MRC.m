1b06f7e66ca39923c1e66076a9611e7e0c36ba14
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-11-02 01:14:38 +0530

    Update BER_MRC.m

--- MATLAB/BER_MRC.m
+++ MATLAB/BER_MRC.m
@@ -1,4 +1,5 @@
-% Bit Error Rate with Maximal Ratio Combining (MRC)
+% Analysis of Bit Error Rate(BER) of receivers with Maximal Radio Combining(MRC) 
+% receiving diversity in frequency-flat and in slowly varying fading channel
 clc;
 close all;
 
@@ -6,7 +7,7 @@ N = 5;
 m = 10^6;
 ip = rand(1, m) > 0.5;
 BPSK = 2*ip-1;
-snr_dB = 0:1:15;
+snr_dB = 0:15;
 snr = 10.^(snr_dB/10);
 L = 2;
 p_R_MRC = 1/2 - 1/2*(1+1./snr).^(-1/2);
@@ -15,10 +16,10 @@ ber_MRC_ana = p_R_MRC.^2.*(1+2*(1-p_R_MRC));
 n_err = zeros(1, length(snr_dB));
 for p = 1:N
     for q = 1:length(snr_dB)
-        No = 1/sqrt(2) *(randn(L,m) + 1j*randn(L,m));
+        N0 = 1/sqrt(2) *(randn(L,m) + 1j*randn(L,m));
         h = 1/sqrt(2)*(randn(L,m)+ 1j*randn(L,m));
         symbol = kron(ones(L,1), BPSK);
-        rec_vector = h.*symbol + 10^(-snr_dB(q)/20)*No;
+        rec_vector = h.*symbol + 10^(-snr_dB(q)/20)*N0;
         dec_metric = sum(conj(h).*rec_vector, 1)./sum(h.*conj(h), 1);
         ip_hat = real(dec_metric) > 0;
         n_err(q) = n_err(q) + size(find(ip-ip_hat), 2);
