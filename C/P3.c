53917900c5b8bf63f29acb69e4e05f5a42e6139d
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-11-02 17:58:32 +0530

    Update P3.c

--- C/P3.c
+++ C/P3.c
@@ -8,15 +8,17 @@
 #define STX 2
 #define ETX 3
 
-char inputCharacter, stuffedData[50], deStuffedData[50];
+char stuffedData[50], deStuffedData[50];
 char Preamble[] = {DLE, STX}, Postamble[] = {DLE, ETX};
-int length = 2;
+int length;
 
 void stuffData()
 {
+    char inputCharacter;
+    length = 2;
+    strcat(stuffedData, Preamble);
     printf("\nEnter the data stream:\n");
     printf("CTRL+P -> DLE\nCTRL+B -> STX\nCTRL+C -> ETX\n");
-    strcat(stuffedData, Preamble);
     while ((inputCharacter = getche()) != '\r')
     {
         if (inputCharacter == DLE)
@@ -25,21 +27,20 @@ void stuffData()
     }
     strcat(stuffedData, Postamble);
     printf("\nCharacter Stuffed Stream:\n");
-    for (int i = 0; i < length + 2; i++)
-        printf("%c", stuffedData[i]);
+    printf("%s\n", stuffedData);
 }
 
 void destuffData()
 {
-    printf("\nDe-Stuffed Stream:\n");
-    for (int j = 2, k = 0; j, k < length; j++, k++)
+    length = 0;
+    for (int i = 2; i < strlen(stuffedData) - 2; i++)
     {
-        if (stuffedData[j] == DLE)
-            j++;
-        deStuffedData[k] = stuffedData[j];
+        if (stuffedData[i] == DLE)
+            i++;
+        deStuffedData[length++] = stuffedData[i];
     }
-    for (int i = 0; i < length - 2; i++)
-        printf("%c", deStuffedData[i]);
+    printf("\nDe-Stuffed Stream:\n");
+    printf("%s\n", deStuffedData);
 }
 
 void main()
