a80596ebe1b9628390570589d2929cc5d610f172
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-12-18 13:36:00 +0530

    Update P5.m

--- MATLAB/P5.m
+++ MATLAB/P5.m
@@ -3,16 +3,15 @@ clc
 close all;
 
 with_shadowing = input("w/ Shadowing? ");
-Fixed_Distance = input("Enter the reference distance: ");
-Arbitrary_Distance = 1e3 : 1e3 : 20e3;
-% Values must be between 1.6 to 6
-Path_Loss_Exponent = rand(1,12)*(6-1.6) + 1.6;
+d0 = input("Enter the reference distance: "); % Fixed_Distance
+d = 1e3 : 1e3 : 20e3;
+n = rand(1,12)*(6-1.6) + 1.6; % Path_Loss_Exponent - Values must be between 1.6 to 6
 Sigma = rand(1,12)*(15-3) + 3;
-Frequency = randi([900, 4000], 1,12);
+f = randi([900, 4000], 1,12);
 X = zeros(1,12);
 
 for i = 1:12
-    Lambda(i) = 3e8 / (Frequency(i) * 10 ^ 6);
+    Lambda(i) = 3e8 / (f(i) * 10 ^ 6);
     PL_D0(i) = 20 * log10((4 * pi * Fixed_Distance) / Lambda(i));
     if(with_shadowing)
         X(i) = randn(size(PL_D0(i))) * Sigma(i);  % X(i) = normrnd(0, Sigma(i));
@@ -20,12 +19,12 @@ for i = 1:12
 end
 for i = 1:12
     for j = 1:20
-        Path_Loss(i, j) = PL_D0(i) + 10 * Path_Loss_Exponent(i) * log10(Arbitrary_Distance(j) / Fixed_Distance)...
+        PL(i, j) = PL_D0(i) + 10 * n(i) * log10(d(j) / d0)... % Path loss
             + X(i);
     end
 end
 
-plot(Arbitrary_Distance, Path_Loss);
+plot(d, Path_Loss);
 legend("Retail Store", "Grocery Store", "Office Hard Partition", "Office Soft Partition", "Textile/Chemical",...
     "Paper/Cereals", "Metal Working", "Indoor Street", "Textile/Chemical", "Metal Working");
 xlabel("Distance in m");
